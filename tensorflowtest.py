e52c23d8a5f9eeb881fc3a2b76991c88ffccaad4
tofuadmiral <fuad98ali@gmail.com>
2018-10-02 19:48:17 -0400

    recasting pixel values as on range 0-1

--- tensorflowtest.py
+++ tensorflowtest.py
@@ -20,3 +20,32 @@ fashionpics = keras.datasets.fashion_mnist
 # each number corresponds to a diff cateogry i.e. dress, etc. 
 
 
+## store the labels bc we don't have them
+label_names = ['T-shirt/top', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+
+print(train_images.shape)
+print(len(train_labels))
+print(len(test_images)) # 6-1 split for train vs test
+
+print(train_labels)
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+
+
+# now we want to scale the values to a range of 0-1
+# before we feed it into the nn model
+# cast integer to float and divide by 255
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# display first 25 images from training set and display class name
+# after this, we'll be ready to build and train
+
+
+
+
+
