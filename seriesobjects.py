c5ee411edd1ffe0e545f1d23b9df6770404ba911
tofuadmiral <fuad98ali@gmail.com>
2018-10-01 21:53:46 -0400

    finally got pandas working

--- seriesobjects.py
+++ seriesobjects.py
@@ -14,4 +14,13 @@ song_arts = {'Nas': 'NY State of Mind', 'Biggie': 'Warning', 'MJ': 'One More Cha
 # pd.Series() creates a series object from the data, and you have
 # to define the data as a parameter. we will create one from the songs list
 ser_num = pd.Series(data=songs)
-ser_num
+
+
+# now make the artist names the indexes
+ser_art = pd.Series(data=songs, index=artists)
+
+
+# now create a series from a dictionary 
+series_dictionary = pd.Series(song_arts)
+print(series_dictionary)
+
