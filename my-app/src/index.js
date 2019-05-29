6c789e23a33499fbd8dd5db6a4c22a2845f95e87
tofuadmiral <fuad98ali@gmail.com>
2019-05-29 19:47:35 -0400

    updated to change state based on click!

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
+    // anything inside curly brackets is JSX code!!! 
+
+    // each square should remember its state, private to that square
+    // set this using the this.state thing 
+    // make a constructor to initialize it
+
+    constructor(props) {
+        super(props); // inherit the supers props, always have to start w this 
+        this.state={
+            value: null, 
+        };
+    }
+
   render() { // draw from CSS to get square classname, and then show value 
+            // use the arrow function notation 
+
+
     return (
-      <button className="square"> 
+      <button className="square" onClick={() => {this.setState({value: 'X'})}}>  
         {/* TODO */
-            this.props.value // shows the value of the square 
+            //this.props.value // shows the value of the square 
+            // show the state 
+            this.state.value
         }
       </button>
     );
